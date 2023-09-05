ff7f558ab1e37640aadd5919f8087d0661db567a
ryewhiskey <shaodwjaylong@gmail.com>
2023-09-05 22:54:33 +0800

    Update minimunSpeedToArriveOnTime.js

--- Base/MinimunSpeedToArriveOnTime_1870/minimunSpeedToArriveOnTime.js
+++ Base/MinimunSpeedToArriveOnTime_1870/minimunSpeedToArriveOnTime.js
@@ -15,8 +15,51 @@ var minSpeedOnTime = function(dist, hour) {
     if (len - 1 >= hour) {
         return -1;
     }
-    let sum = dist.reduce((sum, item) => {return sum + item}) / hour
-    return Math.ceil(sum)
+    let getTime = (i) => {
+        let totalTime = 0;
+        dist.forEach((val, index) => {
+            let time = val / i;
+            if (index == dist.length - 1) {
+
+            } else {
+                time = Math.ceil(time);
+            }
+            totalTime += time
+        })
+        return totalTime;
+    }
+
+    let binarySearch = (left, right) => {
+        return Math.ceil((left + right) / 2);
+    }
+    let left = 1;
+    let right = Math.pow(10, 7);
+    let i = binarySearch(left, right);
+    let time;
+    let lastTime = 0;
+
+    let getSpeed = (lastIndex) => {
+        time = getTime(i);
+        if (time <= hour) {
+            if (time < lastTime) {
+                console.log(time, lastTime)
+                return lastIndex;
+            } else if (time == lastTime) {
+                return i;
+            } else {
+                lastTime = time;
+                right = i;
+                i = binarySearch(left, right);
+                if (right == 2) i = 1;
+                return getSpeed(right);
+            }
+        } else {
+            left = i;
+            i = binarySearch(left, right);
+            return getSpeed(left);
+        }
+    }
+    return getSpeed(i);
 };
 
-console.log(minSpeedOnTime([1,3,2], 1.9))
+console.log(minSpeedOnTime([1,3,2], 6))
